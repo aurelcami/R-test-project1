11e6786e719dac4611c2327a528b12d7dfd9b5f5
Aurel Cami <acami2012@gmail.com>
2018-06-19 18:25:36 -0400

    Added an R script Edited README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # R-test-project1
+This is a project containing some R source code and documentation files.
---
+++ test-prog1.R
@@ -0,0 +1,11 @@
+mean.val <- function(a) {
+  M <- mean(a)
+  return(M)
+}
+
+main.func <- function() {
+  
+  c <- 1:100
+  
+  V <- mean.val(c)
+}
